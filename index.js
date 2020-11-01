3705a145c16c2a01aefc59ff2fd519551ecdc385
wescorner <58126085+wescorner@users.noreply.github.com>
2020-11-01 17:26:12 -0500

    added courses subject codes and descriptions
    added http get methods to add URL's to show the full timetable list, the list of subject codes, and the list of descriptions. Also made functions to ensure that no duplicates were added

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+const jsonTimetable = require('C:/Users/Wesley/Documents/GitHub/se3316-lab3-wcorner/Lab3-timetable-data.json');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+let timetable = JSON.parse(JSON.stringify(jsonTimetable));
+
+var subjects = [];
+var descriptions = [];
+
+timetable.forEach(addSubject);
+timetable.forEach(addDescription);
+
+function addSubject(item){
+    if(subjects.includes(item.subject)){      
+    }
+    else{
+        subjects.push(item.subject)
+    }
+}
+
+function addDescription(item){
+    if(descriptions.includes(item.className)){       
+    }
+    else{
+        descriptions.push(item.className)
+    }
+}
 
 app.get('/', (rec, res) => {
     res.send('Hello World');
 });
 
+app.get('/api/courses', (rec, res) => {
+    res.send(timetable);
+});
+
+app.get('/api/subjectcodes', (rec, res) => {
+    res.send(subjects);
+});
+
+app.get('/api/descriptions', (rec, res) => {
+    res.send(descriptions);
+});
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}...`);
 });
