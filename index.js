2ab530865eaaf14414846b0625767e564c2ea5ad
wescorner <58126085+wescorner@users.noreply.github.com>
2020-11-01 18:14:43 -0500

    updated formatting
    updated code formatting and added some comments

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ const port = 3000;
 
 let timetable = JSON.parse(JSON.stringify(jsonTimetable));
 
-var subjects = [];
-var descriptions = [];
-
-timetable.forEach(addSubject);
-timetable.forEach(addDescription);
-
-function addSubject(item){
-    if(subjects.includes(item.subject)){      
-    }
-    else{
-        subjects.push(item.subject)
-    }
-}
-
-function addDescription(item){
-    if(descriptions.includes(item.className)){       
-    }
-    else{
-        descriptions.push(item.className)
-    }
-}
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -35,24 +13,55 @@ app.get('/api/courses', (req, res) => {
     res.send(timetable);
 });
 
+//for retrieving all subject codes without duplication
 app.get('/api/subjectcodes', (req, res) => {
+
+    var subjects = [];
+
+    timetable.forEach(addSubject);
+
+    function addSubject(item){
+        if(subjects.includes(item.subject)){      
+        }
+        else{
+            subjects.push(item.subject)
+        }
+    }
+
     res.send(subjects);
 });
 
+//for retrieving all descriptions without duplication
 app.get('/api/descriptions', (req, res) => {
+
+    var descriptions = [];
+
+    timetable.forEach(addDescription);
+
+    function addDescription(item){
+        if(descriptions.includes(item.className)){       
+        }
+        else{
+        descriptions.push(item.className)
+        }
+    }
     res.send(descriptions);
 });
 
+//for retrieving all course codes for a given subject code
 app.get('/api/courses/:subjectcode_id', (req, res) => {
     const id = req.params.subjectcode_id;
     
     var coursecodes = [];
+
     timetable.forEach(addCourseCode);
+
     function addCourseCode(item){
         if(id == item.subject){
             coursecodes.push(item.catalog_nbr);
         }
     }
+
     if(coursecodes.length == 0){
         res.status(404).send(`Error- subject code ${id} was not found!`);
     }
@@ -62,6 +71,7 @@ app.get('/api/courses/:subjectcode_id', (req, res) => {
     
 });
 
+//opening port
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
